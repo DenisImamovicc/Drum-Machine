2a7fdb10a6d43f7266799ce4a66120f6bd9effbc
DenisImamovicc <Danielarbete@protonmail.com>
2023-02-02 14:35:35 +0100

    Fix audio delay

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,30 @@ import './App.css'
 function App() {
   const [displayInstrument, setdisplayInstrument] = useState("None")
   const presetDrumSet = [
-    { name: "Heater 1", id: "Q", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
-    { name: "Heater 2", id: "W", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
-    { name: "Heater 3", id: "E", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
-    { name: "Heater 4", id: "A", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" },
-    { name: "Clap", id: "S", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
-    { name: "Open HH", id: "D", src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
-    { name: "Kick_n_Hat", id: "Z", src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
-    { name: "Kick", id: "X", src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
-    { name: "Closed-HH", id: "C", src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" },
+    { name: "Heater 1", id: "Q", src: "src/assets/Heater-1.mp3" },
+    { name: "Heater 2", id: "W", src: "src/assets/Heater-2.mp3" },
+    { name: "Heater 3", id: "E", src: "src/assets/Heater-3.mp3" },
+    { name: "Heater 4", id: "A", src: "src/assets/Heater-4_1.mp3" },
+    { name: "Clap", id: "S", src: "src/assets/Heater-6.mp3" },
+    { name: "Open HH", id: "D", src: "src/assets/Dsc_Oh.mp3" },
+    { name: "Kick_n_Hat", id: "Z", src: "src/assets/Kick_n_Hat.mp3" },
+    { name: "Kick", id: "X", src: "src/assets/RP4_KICK_1.mp3" },
+    { name: "Closed-HH", id: "C", src: "src/assets/RP4_KICK_1.mp3" },
   ]
-  function handlePlayMouseClick(e) {
+  function handlePlayMouseClick(e: any) {
     const button = document.getElementById(`${e.target.id}`)
 
-    console.log(e)
+    // console.log(e)
     const audio = e.target.childNodes[1]
+    audio.currentTime = 0;
     button?.blur();
     setdisplayInstrument(e.target.id)
     return audio.play()
   }
   
-
   function findInstrumentNameByID(id:string) {
     return presetDrumSet.find(instrument=> instrument.id === id)
   }
-
-  console.log(findInstrumentNameByID("X"));
-  
 
   document.addEventListener("keypress", (e: any) => {
     const pressedkey = e.key.toUpperCase()
@@ -38,24 +35,27 @@ function App() {
 
     if (presetDrumSet.find(key=> key.id === pressedkey)) {
       const audio = document.getElementById(`${pressedkey}`)
+      audio.currentTime = 0;
+
       const instrumentName = findInstrumentNameByID(pressedkey)?.name
       setdisplayInstrument(instrumentName)
-       console.log(audio);
+      //  console.log(audio);
       return audio.play()
     }
     return null
   })
 
-
   return (
     <div id='drum-machine'>
       <div id='display'>
-        <div className='displayInstrument'>{displayInstrument}</div>
+        <div id='displayInstrument'>{displayInstrument}</div>
+        <div className='buttons'>
         {presetDrumSet.map((Instrument) => {
           return <div tabIndex={-1} className='drum-pad' onClick={handlePlayMouseClick} id={`${Instrument.name}`}>{Instrument.id}
-            <audio className='clip' id={`${Instrument.id}`} src={`${Instrument.src}`}></audio>
+            <audio preload='auto' className='clip' id={`${Instrument.id}`} src={`${Instrument.src}`}></audio>
           </div>
         })}
+        </div>
       </div>
     </div>
   )
